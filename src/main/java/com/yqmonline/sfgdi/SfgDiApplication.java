3ddc4ce8841f106b3406bc240da3041a3907d216
Austin McGowan <AMcGowan@ShelterInsurance.com>
2020-12-23 12:45:02 -0600

    minor layout change

--- src/main/java/com/yqmonline/sfgdi/SfgDiApplication.java
+++ src/main/java/com/yqmonline/sfgdi/SfgDiApplication.java
@@ -15,9 +15,9 @@ public class SfgDiApplication {
 
 		ApplicationContext ctx  = SpringApplication.run(SfgDiApplication.class, args);
 
-		MyController myController = (MyController) ctx.getBean("myController");
 
 		System.out.println("------- Primary Bean");
+		MyController myController = (MyController) ctx.getBean("myController");
 		System.out.println(myController.sayHello());
 
 		System.out.println("------- Property");
